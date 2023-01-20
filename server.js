d5add7cfcc728a8e9ff991da22cd02cb40a0d0bc
Lil-Jero <jerome.voipierre@gmail.com>
2023-01-20 19:01:14 +0100

    changement cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Authorisation cors
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://jeromevoipierre.fr',
     credentials: true,
     optionsSuccessStatus: 200,
 }))
